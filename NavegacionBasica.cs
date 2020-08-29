61962a21a8eae7888e3c9ca8171f4c1825075a8f
Juan Carlos Campos <rapidclimate@gmail.com>
2020-08-29 10:17:49 -0500

    Add files via upload

---
+++ NavegacionBasica.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavegacionBasica : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform objetivo;
+    void Start()
+    {
+        NavMeshAgent agente = GetComponent<NavMeshAgent>();
+        agente.destination = objetivo.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
