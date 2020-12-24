be657f5a16f9310bd9e457f85632718ad046b60b
Juan Carlos Campos <rapidclimate@gmail.com>
2020-12-23 19:06:46 -0600

    Add files via upload

---
+++ MiFrameRate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiFrameRate : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int target = 60;
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        print("RATE INIIAL " + Application.targetFrameRate);
+        Application.targetFrameRate = target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (target != Application.targetFrameRate)
+        {
+            Application.targetFrameRate = target;
+            print("el rate es " + Application.targetFrameRate);
+        }
+        */
+    }
+}
