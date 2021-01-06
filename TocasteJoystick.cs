c7546f0696215f1ceb33e42465c661c73f5f31f5
Juan Carlos Campos <rapidclimate@gmail.com>
2021-01-06 14:12:38 -0600

    Add files via upload

---
+++ TocasteJoystick.cs
@@ -0,0 +1,30 @@
+﻿using DigitalRubyShared;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TocasteJoystick : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject camaravirtual;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public void activarDosToques()
+    {
+        camaravirtual.GetComponent<FingersRotateCameraComponentScript>().NumberOfTouchesRequired = 2;
+    }
+    public void activarUnToque()
+    {
+        camaravirtual.GetComponent<FingersRotateCameraComponentScript>().NumberOfTouchesRequired = 1;
+    }
+}
