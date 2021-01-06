c8417bc7780c3c3f627dfcb6b7b0b08a67531d20
Juan Carlos Campos <rapidclimate@gmail.com>
2021-01-06 14:16:27 -0600

    Add files via upload

---
+++ AdministrarToque.cs
@@ -0,0 +1,33 @@
+﻿using DigitalRubyShared;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AdministrarToque : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject camaravirtual;
+    public TextMeshProUGUI texto;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void desactivar()
+    {
+      //  camaravirtual.GetComponent<FingersRotateCameraComponentScript>().
+    }
+
+    public void activar()
+    {
+        //camaravirtual.GetComponent<FingersRotateCameraComponentScript>().gameObject.SetActive(true);
+        print("Tocaste este cubito");
+        texto.text = "Tocaste el " + this.transform.gameObject.name;
+    }
+}
