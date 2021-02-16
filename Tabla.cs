8f6ebc4ee268e2257c579c59d193cf923d9eaab1
Juan Carlos Campos <rapidclimate@gmail.com>
2021-02-16 08:59:34 -0600

    Add files via upload

---
+++ Tabla.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public  class Tabla {
+    public string valor;
+    public string resultado;
+
+    public Tabla(string valor, string resultado){
+        this.valor=valor;
+        this.resultado=resultado;
+    }
+}
